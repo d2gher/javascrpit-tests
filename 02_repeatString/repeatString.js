891e9c2e20f2579ae825c7bb59677b1f2bf77310
d2gher <d2gher@gmail.com>
2023-01-16 00:29:03 +0200

    Solve 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(string, n) {
+    if (n < 0) return "ERROR"
+    
+    let output = "" 
+    for (let i = 0; i < n; i++) {
+        output += string;
+    }
+    return output;
 };
 
 // Do not edit below this line
