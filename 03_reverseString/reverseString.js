645e85c00bd7174694c80924941a37e8121c4235
d2gher <d2gher@gmail.com>
2023-01-16 00:36:06 +0200

    Solve 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    // Container for the flipped text
+    let output = "";
+    let len = str.length;
+    // A for loop that runs backwards and adds 
+    // the characters from last to first to the container
+    for (let i = len - 1; i >= 0; i--) {
+        output += str[i];
+    }
+    return output
 };
 
 // Do not edit below this line
