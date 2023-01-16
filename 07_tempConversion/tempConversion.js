515e0827f80d7c91afc05d764385496751bb919b
d2gher <d2gher@gmail.com>
2023-01-16 13:45:00 +0200

    Solve 09_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(f) {
+  // Convert from Fahrenheit to Celsius formula
+  let c = (f - 32) * 5/9;
+  return +c.toFixed(1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(c) {
+  // Convert from Celsius to Fahrenheit formula
+  let f = (c * 9 / 5 + 32);
+  return +f.toFixed(1);
 };
 
 // Do not edit below this line
