591c520002c03a825019a1eb7132c52fb9f5c58a
d2gher <d2gher@gmail.com>
2023-01-16 12:58:29 +0200

    Solve 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,13 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    if (typeof(start) != 'number' || typeof(end) != 'number') return "ERROR";
+    if (start < 0 || end < 0 ) return "ERROR";
 
+    let params = [start, end].sort((a, b) => a-b);
+    let sum = 0;
+    for (let i = params[0]; i <= params[1]; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
