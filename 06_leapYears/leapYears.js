0731b02715cbd109d9799dd46107076b713f57ca
d2gher <d2gher@gmail.com>
2023-01-16 14:40:42 +0200

    Update solution for 06_leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,11 +1,11 @@
 const leapYears = function(year) {
-    if (year % 4 == 0) {
-        if (year % 100 == 0) {
-            if (year % 400 == 0) return true;
-            else return false;
-        } else return true;
-    }
-    return false;
+    // if (year % 4 == 0) {
+    //     if (year % 100 == 0) {
+    //         if (year % 400 == 0) return true;
+    //         else return false;
+    //     } else return true;
+    // }
+    return year % 4 == 0 && (year % 100 != 0 || year % 400 == 0);
 };
 
 // Do not edit below this line
