d7f64d600497d7648171b4b86ede9a6224076104
d2gher <d2gher@gmail.com>
2023-01-20 12:13:08 +0200

    Solve 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,32 @@
-const add = function() {
-	
+const add = function(...args) {
+	const sum = [...args].reduce((total, number) => total + number, 0);
+  return sum;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	const sum = arr.reduce((total, number) => total + number, 0);
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  const sum = arr.reduce((total, number) => total * number, 1);
+  return sum;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let sum = 1;
+	for (let i = 1; i < num; i++) {
+    sum *= (i + 1);
+  }
+  return sum
 };
 
 // Do not edit below this line
