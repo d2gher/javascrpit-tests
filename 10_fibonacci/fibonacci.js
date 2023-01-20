b75ab19cf576df3d46267da8f7429c995c2709b4
d2gher <d2gher@gmail.com>
2023-01-20 14:10:19 +0200

    Solve 10_fibonacci && 11_getTheTitle

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (num < 0) return "OOPS"; 
+    let fibonacciSeq = [];
+    for (let i = 0; i < num; i++) {
+        if (i == 0 || i == 1) fibonacciSeq.push(1)
+        else fibonacciSeq.push((fibonacciSeq[i - 1] + fibonacciSeq[i - 2]));
+    }
+    return fibonacciSeq[num - 1];
 };
 
 // Do not edit below this line
