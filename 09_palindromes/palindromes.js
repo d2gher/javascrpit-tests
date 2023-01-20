934e431254d5848c0a41ec4cfeee50ba6aea9ead
d2gher <d2gher@gmail.com>
2023-01-20 12:45:41 +0200

    Solve 09/palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let cleanStr = str.replace(/[^a-z0-9]/gi, '').toLowerCase()
+    
+    let reversedStr = ""
+    let len = cleanStr.length;
+    for (let i = len - 1; i >= 0; i--) {
+        reversedStr += cleanStr[i];
+    }
+    
+    return cleanStr == reversedStr;
 };
 
 // Do not edit below this line
