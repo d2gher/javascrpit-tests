b17b3abe35817a8026892a612a5a3aafdc085575
d2gher <d2gher@gmail.com>
2023-01-20 15:21:35 +0200

    Solve 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const oldest = people.sort((a, b) => {
+        if (!a.yearOfDeath) aAge = (new Date().getFullYear()) - a.yearOfBirth;
+        else aAge = a.yearOfDeath - a.yearOfBirth;
 
+        if (!b.yearOfDeath) bAge = (new Date().getFullYear()) - b.yearOfBirth;
+        else bAge = b.yearOfDeath - b.yearOfBirth;
+
+        return aAge < bAge? 1 : -1;
+    })
+    return oldest[0]
 };
 
 // Do not edit below this line
